d77d37de852aa1e7df9c2aa2e7cf42e0bb99ff5b
SYdney-Go <ksy960809@gmail.com>
2026-07-02 12:44:32 +0900

    Squashed 'do_it_django_book_version/' content from commit 9db02ca
    git-subtree-dir: do_it_django_book_version
    git-subtree-split: 9db02ca3b24bb880e625ac46f25887267caff9fc

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('delete_comment/<int:pk>/', views.delete_comment),
+    path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
+    path('create_post/', views.PostCreate.as_view()),
+    path('tag/<str:slug>/', views.tag_page),
+    path('category/<str:slug>/', views.category_page),
+    path('<int:pk>/new_comment/', views.new_comment),
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
+]
---
+++ single_pages/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from blog.models import Post
+
+def landing(request):
+    recent_posts = Post.objects.order_by('-pk')[:3]
+    return render(
+        request,
+        'single_pages/landing.html',
+        {
+            'recent_post': recent_posts,
+        }
+    )
+
+def about_me(request):
+    return render(
+        request,
+        'single_pages/about_me.html'
+    )
